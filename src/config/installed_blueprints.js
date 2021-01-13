570cfe5b35bfde498bd70cd679f3add65ecf89ad
Crowdbotics <team@crowdbotics.com>
2021-01-13 03:08:41 +0000

    add package blueprint-react-native-notifications-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'NotificationList192234', human_name: 'Notification List', access_route: 'NotificationList192234'},
 { name: 'Settings192233', human_name: 'Settings', access_route: 'Settings192233'},
 { name: 'Settings192225', human_name: 'Settings', access_route: 'Settings192225'},
 { name: 'UserProfile192223', human_name: 'User Profile', access_route: 'UserProfile192223'},
